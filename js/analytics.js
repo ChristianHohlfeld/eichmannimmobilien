ffee7cef9a4983cf7f50c794042450a38bb57459
Christian Heinrich Hohlfeld <chris.hohlfeld@gmail.com>
2026-09-19 02:24:09 +0000

    Implement SEO, social metadata, schema, and analytics scaffold

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+(function () {
+  var MEASUREMENT_ID = ''; // set to 'G-XXXXXXXX' to enable GA4
+  if (!MEASUREMENT_ID || MEASUREMENT_ID.indexOf('G-') !== 0) return;
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', MEASUREMENT_ID);
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(MEASUREMENT_ID);
+  document.head.appendChild(s);
+})();
